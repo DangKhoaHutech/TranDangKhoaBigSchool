bbcf3cc9b7ce99219a94e143ea5ba504fa38ec44
khoa <khoa@DESKTOP-TN7OONQ>
2019-04-05 15:03:25 +0700

    3:03 PM Page 51

---
+++ BigSchool_1611060152/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BigSchool_1611060152.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool_1611060152.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
